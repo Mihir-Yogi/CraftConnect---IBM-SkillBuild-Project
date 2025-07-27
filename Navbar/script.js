554aca8afdf65199350990f8449e92b41d257733
MaitriSoni17 <maitrisoni171@gmail.com>
2025-07-27 17:22:45 +0530

    Navbar and Footer Updated

--- Navbar/script.js
+++ Navbar/script.js
@@ -1,29 +1,14 @@
-const mybutton = document.getElementById("backToTopBtn");  
-  window.onscroll = function () {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  };
-
-  
-  function topFunction() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+const mybutton = document.getElementById("backToTopBtn");
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+};
 
-const container = document.querySelector('.card-container');
-const cards = document.querySelectorAll('.card');
-const prev = document.querySelector('.prev-btn');
-const next = document.querySelector('.next-btn');
 
-const cardStyle = window.getComputedStyle(cards[0]);
-const cardWidth = cards[0].offsetWidth + parseInt(cardStyle.marginRight || 20);
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
-next.addEventListener('click', () => {
-  container.scrollLeft += cardWidth * 1; // scroll by 1 card
-});
-
-prev.addEventListener('click', () => {
-  container.scrollLeft -= cardWidth * 1;
-});
